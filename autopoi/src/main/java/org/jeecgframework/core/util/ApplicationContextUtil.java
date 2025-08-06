b35612dabd03fcb92aced0b5d331f0f823e6511f
JEECG <445654970@qq.com>
2025-08-06 17:40:49 +0800

    【lazy-initialization】声明使用ApplicationContext的非懒加载，启动就注册

--- autopoi/src/main/java/org/jeecgframework/core/util/ApplicationContextUtil.java
+++ autopoi/src/main/java/org/jeecgframework/core/util/ApplicationContextUtil.java
@@ -3,21 +3,26 @@ package org.jeecgframework.core.util;
 import org.springframework.beans.BeansException;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationContextAware;
+import org.springframework.context.annotation.Lazy;
+import org.springframework.stereotype.Component;
 
 /**
  * 
  * @author  张代浩
  *
  */
+@Lazy(false)
+@Component
 public class ApplicationContextUtil implements ApplicationContextAware {
 
 	private static ApplicationContext context;
 
-	
-	public void setApplicationContext(ApplicationContext context)
-			throws BeansException {
-		this.context = context;
+
+	@Override
+	public void setApplicationContext(ApplicationContext context) throws BeansException {
+		ApplicationContextUtil.context = context;
 	}
+
 
 	public static ApplicationContext getContext() {
 		return context;
