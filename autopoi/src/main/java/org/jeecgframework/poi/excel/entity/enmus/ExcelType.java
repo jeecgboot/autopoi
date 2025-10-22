34977e72436e3da0b35b60217462f866efa7ba3b
JEECG <445654970@qq.com>
2025-10-22 17:18:28 +0800

    完善ExcelType枚举类的文档注释，增加HSSF和XSSF格式的详细说明

--- autopoi/src/main/java/org/jeecgframework/poi/excel/entity/enmus/ExcelType.java
+++ autopoi/src/main/java/org/jeecgframework/poi/excel/entity/enmus/ExcelType.java
@@ -16,13 +16,38 @@
 package org.jeecgframework.poi.excel.entity.enmus;
 
 /**
- * Excel Type
+ * Excel 文件格式类型枚举
+ * <p>用于指定导出/导入的 Excel 文件格式版本</p>
  * 
  * @author JEECG
  * @date 2014年12月29日 下午9:08:21
  */
 public enum ExcelType {
 
-	HSSF, XSSF;
+	/**
+	 * HSSF 格式 - Excel 97-2003 版本 (.xls)
+	 * <ul>
+	 *   <li>文件扩展名：.xls</li>
+	 *   <li>最大行数：65,536 行（2^16）</li>
+	 *   <li>最大列数：256 列（2^8）</li>
+	 *   <li>适用场景：兼容老版本 Excel，数据量较小的场景</li>
+	 *   <li>对应 POI 类：HSSFWorkbook</li>
+	 * </ul>
+	 */
+	HSSF,
+	
+	/**
+	 * XSSF 格式 - Excel 2007+ 版本 (.xlsx)
+	 * <ul>
+	 *   <li>文件扩展名：.xlsx</li>
+	 *   <li>最大行数：1,048,576 行（2^20）</li>
+	 *   <li>最大列数：16,384 列（2^14）</li>
+	 *   <li>适用场景：现代 Excel 版本，大数据量导出，推荐使用</li>
+	 *   <li>对应 POI 类：XSSFWorkbook</li>
+	 *   <li>优势：支持更大数据量，文件压缩比更高，功能更丰富</li>
+	 * </ul>
+	 * <p><b>注意：</b>导出时请确保文件扩展名与格式类型匹配，避免文件损坏</p>
+	 */
+	XSSF;
 
 }
